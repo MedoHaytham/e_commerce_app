39391f7f2ca8ceeb78e6c7fee72a129d407ab9c9
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2024-02-06 07:15:07 +0200

    testo 0

--- lib/layout/e_commerce_layout.dart
+++ lib/layout/e_commerce_layout.dart
@@ -6,8 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EcommerceLayout extends StatelessWidget {
 
-  //helllo
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
